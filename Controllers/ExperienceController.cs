04e6d693d78826a8241e46813bb54ee136209920
ZapDark <57185831+ZapDark@users.noreply.github.com>
2022-11-13 13:33:39 -0500

    Added Pages

---
+++ Controllers/ExperienceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioWebsite.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
